41de78cfe7f94d8d3ffb64be0301865270a523d0
Luke Martin <me@luke.dev>
2021-09-08 08:39:18 +0100

    VisuallyHidden header

---
+++ src/v2/system/components/VisuallyHidden.tsx
@@ -0,0 +1,3 @@
+import * as BaseVisuallyHidden from '@radix-ui/react-visually-hidden';
+
+export const VisuallyHidden = BaseVisuallyHidden.Root;
--- src/v2/system/components/index.ts
+++ src/v2/system/components/index.ts
@@ -3,3 +3,4 @@ export * from './Box';
 export * from './Flex';
 export * from './Text';
 export * from './Button';
+export * from './VisuallyHidden';
